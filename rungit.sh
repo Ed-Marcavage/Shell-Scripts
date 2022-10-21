5a114227123b0afe66ca86dd2317e441d59e0772
Ed-Marcavage <edmarcavage256@proton.me>
2022-10-20 22:01:42 -0400

    test

--- rungit.sh
+++ rungit.sh
@@ -8,13 +8,8 @@ git commit -m $message
 while true; do
     read -p "Push to Main? " yn
     case $yn in
-        [Yy]* ) git push origin main; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) git push origin main; osascript -e 'display notification "pushed to remote"'; break;;
+        [Nn]* ) read -p 'branch: ' branch; git push origin $branch; osascript -e 'display notification "pushed to remote"'; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
-
-read -p 'branch: ' branch
-git push origin $branch
-
-osascript -e 'display notification "pushed to remote"'
